af096967d33308c4de2a0e6b8a96bef8732e254e
sabsilva246-lang <sabsilva246@gmail.com>
2026-03-16 10:54:38 -0300

    Add files via upload

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import *
+from django.contrib import admin
+admin.site.register(Cidade)
+admin.site.register(Autor)
+admin.site.register(Editora)
+admin.site.register(Leitor)
+admin.site.register(Livro)
+admin.site.register(Genero)
